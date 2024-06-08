df4946b46b227b84ce0ab7c857a144afd2c981d3
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2024-06-08 20:16:38 +0200

    fix auto download for spell check languages

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,3 @@
--- Disable as per documentation of nvim-treeini
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
