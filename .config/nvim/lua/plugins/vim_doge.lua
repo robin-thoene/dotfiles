d06b228e0d4c843deb7ecd34ad3c5f354c85ccbc
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2024-09-21 20:41:00 +0200

    WIP: auto setup vim doge after installation

--- .config/nvim/lua/plugins/vim_doge.lua
+++ .config/nvim/lua/plugins/vim_doge.lua
@@ -1,9 +1,6 @@
 return {
   {
     'kkoomen/vim-doge',
-    build = function()
-      -- Ensure the install function is being called after the first install and after updates
-      -- vim.fn[':call doge#install()']()
-    end,
+    build = ':call doge#install()',
   },
 }
