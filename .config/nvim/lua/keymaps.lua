45a9a80912f0acf094c0260c13fad14db9f79f7c
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2024-09-23 14:49:35 +0200

    WIP: add file tree

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -17,7 +17,9 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 -- [[ Telescope ]]
 local telescope_builtin = require 'telescope.builtin'
-vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>sf', telescope_builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>sg', telescope_builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>sb', telescope_builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>sh', telescope_builtin.help_tags, { desc = 'Telescope help tags' })
+-- [[ nvim-tree ]]
+vim.keymap.set('n', '<C-b>', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle file explorer' })
