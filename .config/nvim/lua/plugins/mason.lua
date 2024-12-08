57cafc169272c3304314c9591b988867c7d65523
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2024-12-08 21:53:34 +0100

    add go setup to neovim

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -21,6 +21,7 @@ local lsps = {
 local debuggers = {
   'codelldb',
   'netcoredbg',
+  'delve',
 }
 vim.list_extend(ensure_installed, formatters)
 vim.list_extend(ensure_installed, lsps)
--- .config/nvim/lua/plugins/nvim_dap.lua
+++ .config/nvim/lua/plugins/nvim_dap.lua
@@ -20,6 +20,49 @@ return {
       }
       -- Setup the lldb/codelldb debugger
       dap.adapters.lldb = dap.adapters.codelldb
+      -- Go
+      dap.adapters.delve = function(callback, config)
+        if config.mode == 'remote' and config.request == 'attach' then
+          callback {
+            type = 'server',
+            host = config.host or '127.0.0.1',
+            port = config.port or '38697',
+          }
+        else
+          callback {
+            type = 'server',
+            port = '${port}',
+            executable = {
+              command = 'dlv',
+              args = { 'dap', '-l', '127.0.0.1:${port}', '--log', '--log-output=dap' },
+              detached = vim.fn.has 'win32' == 0,
+            },
+          }
+        end
+      end
+      dap.configurations.go = {
+        {
+          type = 'delve',
+          name = 'Debug',
+          request = 'launch',
+          program = '${file}',
+        },
+        {
+          type = 'delve',
+          name = 'Debug test', -- configuration for debugging test files
+          request = 'launch',
+          mode = 'test',
+          program = '${file}',
+        },
+        -- works with go.mod packages and sub packages
+        {
+          type = 'delve',
+          name = 'Debug test (go.mod)',
+          request = 'launch',
+          mode = 'test',
+          program = './${relativeFileDirname}',
+        },
+      }
       -- Customize the dap ui
       dapui.setup {
         icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
