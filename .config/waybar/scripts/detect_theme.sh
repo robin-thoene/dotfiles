e9cb53b1f3a5f82f29b03de404e7626ad22dd03d
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2025-05-27 10:15:23 +0200

    use device automation scripts

--- .config/waybar/scripts/detect_theme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-current=$(gsettings get org.gnome.desktop.interface color-scheme)
-if [[ "$current" == "'prefer-dark'" ]]; then
-	echo "{\"alt\":\"dark\"}"
-else
-	echo "{\"alt\":\"light\"}"
-fi
